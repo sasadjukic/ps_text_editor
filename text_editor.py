c3f3d8140a600ab870885f11a580d4a5821712cc
sasadjukic <djukicmsasa@gmail.com>
2025-12-11 13:19:31 +0100

    add shortcuts

--- text_editor.py
+++ text_editor.py
@@ -1,8 +1,9 @@
 import sys
 from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QPlainTextEdit, QFileDialog, QMessageBox, QToolBar
+    QApplication, QMainWindow, QPlainTextEdit, QFileDialog, QMessageBox, QToolBar,
+    QToolButton, QMenu
 )
-from PySide6.QtGui import QAction
+from PySide6.QtGui import QAction, QKeySequence
 from PySide6.QtCore import Qt
 
 
@@ -16,21 +17,51 @@ class TextEditor(QMainWindow):
         self.editor = QPlainTextEdit()
         self.setCentralWidget(self.editor)
 
+        # create actions first so toolbar can reuse them
+        self.create_actions()
         self.create_toolbar()
         self.current_file = None
+
+    def create_actions(self):
+    # Open File
+        self.open_action = QAction("Open...", self)
+        self.open_action.setShortcut(QKeySequence.Open)  # Ctrl+O
+        self.open_action.triggered.connect(self.open_file)
+
+        # Save File
+        self.save_action = QAction("Save", self)
+        self.save_action.setShortcut(QKeySequence.Save)  # Ctrl+S
+        self.save_action.triggered.connect(self.save_file)
+
+        # Save As
+        self.save_as_action = QAction("Save As...", self)
+        self.save_as_action.setShortcut(QKeySequence.SaveAs)  # Ctrl+Shift+S
+        self.save_as_action.triggered.connect(self.save_file_as)
+
+        # Close File
+        self.close_action = QAction("Close", self)
+        self.close_action.setShortcut(QKeySequence.Close)  # Ctrl+W
+        self.close_action.triggered.connect(self.close_file)
+
+    # We no longer create a top menu bar; the File menu is a drop-down on the toolbar
 
     def create_toolbar(self):
         toolbar = QToolBar()
         toolbar.setMovable(False)
         self.addToolBar(toolbar)
-
-        open_action = QAction("Open", self)
-        open_action.triggered.connect(self.open_file)
-        toolbar.addAction(open_action)
-
-        save_action = QAction("Save", self)
-        save_action.triggered.connect(self.save_file)
-        toolbar.addAction(save_action)
+        # Add a File drop-down button to the toolbar using the actions created in create_actions
+        file_button = QToolButton(self)
+        file_button.setText("File")
+        file_menu = QMenu(self)
+        file_menu.addAction(self.open_action)
+        file_menu.addAction(self.save_action)
+        file_menu.addAction(self.save_as_action)
+        file_menu.addSeparator()
+        file_menu.addAction(self.close_action)
+        file_button.setMenu(file_menu)
+        # Make button show the menu immediately on click
+        file_button.setPopupMode(QToolButton.InstantPopup)
+        toolbar.addWidget(file_button)
 
     def open_file(self):
         path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt)")
@@ -54,6 +85,17 @@ class TextEditor(QMainWindow):
                 file.write(self.editor.toPlainText())
         except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
+
+    def save_file_as(self):
+        path, _ = QFileDialog.getSaveFileName(self, "Save File As", "", "Text Files (*.txt);;All Files (*)")
+        if not path:
+            return
+        self.current_file = path
+        self.save_file()
+
+    def close_file(self):
+        self.editor.clear()
+        self.current_file = None
 
 def load_stylesheet(app, path):
     with open(path, "r") as f:
