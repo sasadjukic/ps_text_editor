2568c692d9b9abf5a59cf11377f8f4ec6f4b0aa4
sasadjukic <djukicmsasa@gmail.com>
2025-12-12 10:59:40 +0100

    add edit tab

--- text_editor.py
+++ text_editor.py
@@ -43,6 +43,43 @@ class TextEditor(QMainWindow):
         self.close_action.setShortcut(QKeySequence.Close)  # Ctrl+W
         self.close_action.triggered.connect(self.close_file)
 
+        # --- Edit actions ---
+        # We'll track the last edit-related action so "Repeat" can re-run it
+        self._last_edit_action = None
+
+        self.copy_action = QAction("Copy", self)
+        self.copy_action.setShortcut(QKeySequence.Copy)  # Ctrl+C
+        self.copy_action.triggered.connect(self._on_copy)
+
+        self.paste_action = QAction("Paste", self)
+        self.paste_action.setShortcut(QKeySequence.Paste)  # Ctrl+V
+        self.paste_action.triggered.connect(self._on_paste)
+
+        self.cut_action = QAction("Cut", self)
+        self.cut_action.setShortcut(QKeySequence.Cut)  # Ctrl+X
+        self.cut_action.triggered.connect(self._on_cut)
+
+        self.undo_action = QAction("Undo", self)
+        self.undo_action.setShortcut(QKeySequence.Undo)  # Ctrl+Z
+        self.undo_action.triggered.connect(self._on_undo)
+
+        # Redo: use Ctrl+Y
+        self.redo_action = QAction("Redo", self)
+        self.redo_action.setShortcut(QKeySequence("Ctrl+Y"))  # Ctrl+Y
+        self.redo_action.triggered.connect(self._on_redo)
+
+        # Repeat: Shift+Ctrl+Y
+        self.repeat_action = QAction("Repeat", self)
+        self.repeat_action.setShortcut(QKeySequence("Ctrl+Shift+Y"))  # Shift+Ctrl+Y
+        self.repeat_action.triggered.connect(self._on_repeat)
+
+        # Initial enable/disable states
+        self.copy_action.setEnabled(False)
+        self.cut_action.setEnabled(False)
+        # Undo/redo availability will be driven by document signals
+        self.undo_action.setEnabled(False)
+        self.redo_action.setEnabled(False)
+
     # We no longer create a top menu bar; the File menu is a drop-down on the toolbar
 
     def create_toolbar(self):
@@ -62,6 +99,80 @@ class TextEditor(QMainWindow):
         # Make button show the menu immediately on click
         file_button.setPopupMode(QToolButton.InstantPopup)
         toolbar.addWidget(file_button)
+
+        # Add an Edit drop-down button next to File
+        edit_button = QToolButton(self)
+        edit_button.setText("Edit")
+        edit_menu = QMenu(self)
+        edit_menu.addAction(self.copy_action)
+        edit_menu.addAction(self.paste_action)
+        edit_menu.addAction(self.cut_action)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.undo_action)
+        edit_menu.addAction(self.redo_action)
+        edit_menu.addSeparator()
+        edit_menu.addAction(self.repeat_action)
+        edit_button.setMenu(edit_menu)
+        edit_button.setPopupMode(QToolButton.InstantPopup)
+        toolbar.addWidget(edit_button)
+
+        # Connect editor signals to enable/disable actions based on context
+        # copyAvailable(bool) is emitted when a selection is present
+        self.editor.copyAvailable.connect(self.copy_action.setEnabled)
+        self.editor.copyAvailable.connect(self.cut_action.setEnabled)
+
+        # Document signals for undo/redo availability
+        doc = self.editor.document()
+        try:
+            doc.undoAvailable.connect(self.undo_action.setEnabled)
+            doc.redoAvailable.connect(self.redo_action.setEnabled)
+        except Exception:
+            # In case the API differs, fallback to checking availability manually
+            pass
+
+        # Keep the UI in sync at startup
+        self.copy_action.setEnabled(bool(self.editor.textCursor().hasSelection()))
+        self.cut_action.setEnabled(bool(self.editor.textCursor().hasSelection()))
+        self.undo_action.setEnabled(doc.isUndoAvailable())
+        self.redo_action.setEnabled(doc.isRedoAvailable())
+
+    # --- Edit action handlers ---
+    def _on_copy(self):
+        self.editor.copy()
+        self._last_edit_action = "copy"
+
+    def _on_paste(self):
+        self.editor.paste()
+        self._last_edit_action = "paste"
+
+    def _on_cut(self):
+        self.editor.cut()
+        self._last_edit_action = "cut"
+
+    def _on_undo(self):
+        self.editor.undo()
+        self._last_edit_action = "undo"
+
+    def _on_redo(self):
+        self.editor.redo()
+        self._last_edit_action = "redo"
+
+    def _on_repeat(self):
+        # Re-run the last edit action when possible
+        action = self._last_edit_action
+        if not action:
+            return
+        if action == "copy":
+            self.editor.copy()
+        elif action == "paste":
+            self.editor.paste()
+        elif action == "cut":
+            self.editor.cut()
+        elif action == "undo":
+            self.editor.undo()
+        elif action == "redo":
+            self.editor.redo()
+        # Keep last action as repeat-able
 
     def open_file(self):
         path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt)")
